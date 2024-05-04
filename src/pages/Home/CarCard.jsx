fe522def162899d8e2d312013aabc59520b35883
Rasyad Jabbar Poncosewoyo <rasyadponcosewoyo@gmail.com>
2024-05-04 22:53:56 +0700

    tried to modify car card component

--- src/pages/Home/CarCard.jsx
+++ src/pages/Home/CarCard.jsx
@@ -1,4 +1,4 @@
-import {Card} from "react-bootstrap";
+import {Card, CardText} from "react-bootstrap";
 import placeholderPhoto from "../../assets/placeholder_img.svg";
 
 export default function CarCard({props}) {
@@ -7,7 +7,8 @@ export default function CarCard({props}) {
             <Card style={{width: '18rem'}}>
                 <Card.Img variant="top" className={"img-fluid"} src={props.image ?? placeholderPhoto} rounded/>
                 <Card.Body>
-                    <Card.Title>{`${props.model} - ${props.type}`}</Card.Title>
+                    <Card.Title>{`${props.model} / ${props.type}`}</Card.Title>
+                    <Card.Subtitle>{`Rp. ${props.rentPerDay} / hari`}</Card.Subtitle>
                     <Card.Text>
                         {(props.description) ? props.description : "No description provided for this car."}
                     </Card.Text>
